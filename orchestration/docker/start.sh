a652086f73580ca5bf1c3ae37ae42cef114be4d4
jonathangoulding <jonathangoulding21@gmail.com>
2022-03-03 18:44:56 +0000

    ci: docker orchestration pipeline

--- orchestration/docker/start.sh
+++ orchestration/docker/start.sh
@@ -3,5 +3,7 @@
 cd "$(dirname "$0")" || exit
 
 export REGISTRY="ghcr.io/jonathangoulding/orchestration"
+# Default to main - will switch depending on branch
+export TAG="main"
 
 docker-compose up -d

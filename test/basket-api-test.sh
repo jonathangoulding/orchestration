2903aafae936f210d245f3a67f79e48bdae6cd02
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2022-03-04 13:42:13 +0000

    feat: add npm test pack (#2)
    * feat: add npm test pack
    
    add package json for test pack
    
    update pipelines to use npm test pack

--- test/basket-api-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Running Basket Api tests"
-curl -H "Accept: application/json" "http://localhost:3000/basket"
--- test/plant-api-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Running Plant Api tests"
-curl -H "Accept: application/json" "http://localhost:3001/plant"

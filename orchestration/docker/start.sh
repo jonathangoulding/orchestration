7ea857d4b9cca8abc20f658da98053962930e62b
jonathangoulding <jonathangoulding21@gmail.com>
2022-03-04 12:51:45 +0000

    feat: add multiple branch workflow

--- orchestration/docker/start.sh
+++ orchestration/docker/start.sh
@@ -6,6 +6,8 @@ export REGISTRY="ghcr.io/jonathangoulding/orchestration"
 export DEFAULT_TAG="main"
 export TAG="${1:-$DEFAULT_TAG}"
 
+echo "TAG = $TAG"
+
 function GET_TAG() {
   # Check Tag exists if not default to default tag
   docker manifest inspect "$REGISTRY/$1:$TAG" > /dev/null \
@@ -15,4 +17,4 @@ function GET_TAG() {
 export BASKET_API_TAG=$(GET_TAG "basket-api")
 export PLANT_API_TAG=$(GET_TAG "plant-api")
 
-docker-compose up -d
+#docker-compose up -d
